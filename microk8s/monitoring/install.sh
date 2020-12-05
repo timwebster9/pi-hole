5d0522515473e815315e6137ef522b089446201a
Tim Webster <tim.webster@gmail.com>
2020-12-05 18:58:25 +0000

    update grafana

--- microk8s/monitoring/install.sh
+++ microk8s/monitoring/install.sh
@@ -1,3 +1,3 @@
-kubectl --kubeconfig=/Users/timw/.kube/triton.config create ns monitoring
-helm --kubeconfig=/Users/timw/.kube/triton.config install prometheus stable/prometheus --namespace monitoring -f prometheus.yaml
-helm --kubeconfig=/Users/timw/.kube/triton.config install grafana stable/grafana --namespace monitoring -f grafana.yaml
+kubectl --kubeconfig=/home/timw/.kube/triton.config create ns monitoring
+helm --kubeconfig=/home/timw/.kube/triton.config install prometheus stable/prometheus --namespace monitoring -f prometheus.yaml
+helm --kubeconfig=/home/timw/.kube/triton.config install grafana stable/grafana --namespace monitoring -f grafana.yaml
